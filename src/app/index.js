8db1bcd9c4dd31413d168c021a9d8234995552c4
Paritosh Mishra <paritosh149@outlook.com>
2021-01-31 11:53:46 +1100

    First HTML response retrieved

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,26 @@
+const CONTENT_TYPE_TEXT_HTML = 'text/html'
 class UrlToChartImage {
     constructor() {
-        
+
+    }
+
+    fetch = (url) => {
+        const axios = require("axios");
+        const getData = async url => {
+        try {
+            const response = await axios.get(url);
+            const contentType = response.headers['content-type'].toLowerCase()
+            if(!contentType.startsWith(CONTENT_TYPE_TEXT_HTML)) {
+                throw 'Response is not HTML. Cannot convert'
+            }
+            const data = response.data;
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+        };
+
+        getData(url);
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -2,3 +2,9 @@ const App = require('./app/index.js')
 const AppInstance = new App()
 
 console.log('App instance ready...')
+
+// const url = "https://jsonplaceholder.typicode.com/posts/1";
+const url = "https://en.wikipedia.org/wiki/Long_jump";
+
+
+AppInstance.fetch(url)
