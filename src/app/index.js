782cb892341ca8b86c41d7dcf3a1dd17aef325c9
Paritosh Mishra <paritosh149@outlook.com>
2021-02-01 00:25:40 +1100

    Full flow initial test ok

--- src/app/index.js
+++ src/app/index.js
@@ -1,62 +1,110 @@
 const CONTENT_TYPE_TEXT_HTML = 'text/html'
 class UrlToChartImage {
-    constructor() {
+  constructor() {
 
+  }
+
+  fetch = async url => {
+    const axios = require("axios");
+    const response = await axios.get(url);
+    const contentType = response.headers['content-type'].toLowerCase()
+    if (!contentType.startsWith(CONTENT_TYPE_TEXT_HTML)) {
+      throw 'Response is not HTML. Cannot convert.'
     }
+    return response.data;
+  }
 
-    fetch = async url => {
-            const axios = require("axios");
-            const response = await axios.get(url);
-            const contentType = response.headers['content-type'].toLowerCase()
-            if(!contentType.startsWith(CONTENT_TYPE_TEXT_HTML)) {
-                throw 'Response is not HTML. Cannot convert.'
-            }
-            return response.data;
-        }
+  extractHtmlTables = (doc) => {
+    const cheerio = require("cheerio");
+    const $ = cheerio.load(doc);
+    const scrapedTables = [];
 
-    extractHtmlTables = (doc) => {
-        const cheerio = require("cheerio");
-        const $ = cheerio.load(doc);
-        const scrapedTables = [];
-        
-        $("table.wikitable").each((index1, element1) => {
-            const scrapedData = [];
-            const tableHeaders = [];
-            const isHeaderFound = false;
-            $('thead > tr', element1).first((index, element) => {
-                    isHeaderFound = true
-                  const ths = $(element).find("th");
-                  $(ths).each((i, element) => {
-                    tableHeaders.push(
-                      $(element)
-                        .text()
-                        .toLowerCase()
-                    );
-                })
-            })
-            $('tbody > tr', element1).each((index, element) => {
-            if (!isHeaderFound && index === 0) {
-              const ths = $(element).find("th");
-              $(ths).each((i, element) => {
-                tableHeaders.push(
-                  $(element)
-                    .text()
-                    .toLowerCase()
-                );
-              });
-              return true;
-            }
-            const tds = $(element).find("td");
-            const tableRow = {};
-            $(tds).each((i, element) => {
-              tableRow[i] = $(element).text();
-            });
-            scrapedData.push(tableRow);
-          });
-          scrapedTables.push(scrapedData)
-        })
-          console.log(scrapedTables);
-    }
+    $("table.wikitable").each((index1, element1) => {
+      const scrapedData = [];
+      $('tbody > tr', element1).each((index, element) => {
+        const tds = $(element).find("td");
+        const tableRow = [];
+        $(tds).each((i, element) => {
+          const text = $(element).text().trim().replace('\n','');
+          if (text.length > 0)
+            tableRow.push(text);
+        });
+        if(tableRow.length > 0)
+          scrapedData.push(tableRow);
+      });
+      if(scrapedData.length > 0)
+        scrapedTables.push(scrapedData)
+    })
+    return scrapedTables;
+  }
+
+  chart = (labels, dataPoints) => {
+    const { ChartJSNodeCanvas } = require('chartjs-node-canvas');
+
+    const fs = require('fs')
+    const width = Math.max(labels.length * 20, 400); //px
+    const height = 400; //px
+
+    const chartJSNodeCanvas = new ChartJSNodeCanvas({ width, height });
+
+    const configuration = {
+      type: 'line',
+      data: {
+        labels,
+        datasets: [{
+          label: '',
+          data: dataPoints,
+          // backgroundColor: [
+          //     'rgba(255, 99, 132, 0.2)',
+          //     'rgba(54, 162, 235, 0.2)',
+          //     'rgba(255, 206, 86, 0.2)',
+          //     'rgba(75, 192, 192, 0.2)',
+          //     'rgba(153, 102, 255, 0.2)',
+          //     'rgba(255, 159, 64, 0.2)'
+          // ],
+          // borderColor: [
+          //     'rgba(255,99,132,1)',
+          //     'rgba(54, 162, 235, 1)',
+          //     'rgba(255, 206, 86, 1)',
+          //     'rgba(75, 192, 192, 1)',
+          //     'rgba(153, 102, 255, 1)',
+          //     'rgba(255, 159, 64, 1)'
+          // ],
+          // borderWidth: 1
+        }]
+      },
+      // options: {
+      //     scales: {
+      //         yAxes: [{
+      //             ticks: {
+      //                 beginAtZero: true,
+      //                 callback: (value) => '$' + value
+      //             }
+      //         }]
+      //     }
+      // },
+      // plugins: {
+      //     annotation: {
+      //     }
+      // }
+    };
+    const mimeType = 'image/png'
+    const stream = chartJSNodeCanvas.renderToStream(configuration, mimeType);
+    const data = [];
+    stream.on('data', (chunk) => {
+      data.push(chunk);
+    });
+    stream.on('end', () => {
+      // console.log("End: "+data);
+      fs.writeFileSync("test.png", Buffer.concat(data))
+    });
+    stream.on('finish', () => {
+      // console.log("Finished: " + data);
+    });
+    stream.on('error', (error) => {
+
+    });
+  }
 }
 
 module.exports = UrlToChartImage
--- src/index.js
+++ src/index.js
@@ -3,11 +3,42 @@ const AppInstance = new App()
 
 console.log('App instance ready...')
 
-// const url = "https://jsonplaceholder.typicode.com/posts/1";
-const url = "https://en.wikipedia.org/wiki/Long_jump";
+// const url = "https://en.wikipedia.org/wiki/Long_jump";
+
+const url = "https://en.wikipedia.org/wiki/Women%27s_high_jump_world_record_progression";
 
 
 AppInstance.fetch(url).then(data => {
     console.log('Fetched ' + data.length)
-    AppInstance.extractHtmlTables(data)
+    const numeral = require('numeral')
+    const tables = AppInstance.extractHtmlTables(data)
+    tables.every(table => {
+        // First column is assumed to be labels
+
+        // Search for first numeric column
+        const cols = table[0].length;
+        let isValidTable = false
+        let numericValues = []
+        let indexOfValues = 0;
+        for (indexOfValues = 0; indexOfValues < cols; indexOfValues++) {
+            numericValues = table.map(row => numeral(row[indexOfValues].match(/[\d\.]+/)).value())
+            isValidTable = numericValues.every(val => !isNaN(val) && val !== null)
+            if (isValidTable)
+                break;
+        }
+        let indexOfLabels = 0;
+        let labels;
+        for (indexOfLabels = 0; indexOfLabels < cols; indexOfLabels++) {
+            if (indexOfLabels !== indexOfValues) {
+                labels = table.map(row => row[indexOfLabels])
+                break;
+            }
+        }
+        if (isValidTable) {
+            AppInstance.chart(labels, numericValues)
+            // Exit loop after emiting first valid chart
+            return false
+        }
+        return true
+    })
 })
